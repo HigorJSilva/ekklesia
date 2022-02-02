56c30a1dde85a56a80dcf2d4daf2e1c9a98b0a91
Higor Silva <higor.junio@hotmail.com>
2022-02-01 23:50:06 -0300

    alteração de comando de criar controller e alterações de stubs

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -4,15 +4,38 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\BaseController;
 use App\Http\Requests\TesteRequest;
-use App\Services\TesteService;
+use App\Models\Teste;
+use Illuminate\Support\Facades\Auth;
 
+/**
+ * Classes do Api resource estão em BaseController.
+ * Adicione novas requisições conforme o exmplo
+ *
+ * 
+ */
 class TesteController extends BaseController
 {
-    protected $service;
-    protected $request = TesteRequest::class;
 
-    public function __construct(TesteService $service)
+    protected $request = TesteRequest::class;
+    /**
+     * Prepara o request com o id do usuário logado.
+     *
+     * @return void
+     */
+
+    protected function prepareData($data)
     {
-        $this->service = $service;
+        $data['userId'] = Auth::user()->id;
+        return $data;
     }
+
+    /**
+     * Exemplo de nova requisicao
+     *
+     * @return \Illuminate\Http\Response
+     */
+    //public function importar()
+    //{
+    //return jsonDefaultResponse($this->service->importarXls(request()->file('file')));
+    //}
 }
